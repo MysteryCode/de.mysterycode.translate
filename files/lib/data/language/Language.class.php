2fc15527e0559e4a63e0fb38fcb8fe0ae9f4e56d
mutec <webmaster@mysterycode.de>
2016-08-19 22:20:16 +0200

    Language::getSystemLanguage()
    fetches the wcf\data\language\Language object by mathing it's
    languageCode property

--- files/lib/data/language/Language.class.php
+++ files/lib/data/language/Language.class.php
@@ -3,9 +3,14 @@
 namespace translate\data\language;
 use wcf\data\DatabaseObject;
 use wcf\system\WCF;
+use wcf\system\language\LanguageFactory;
 
 class Language extends DatabaseObject {
 	public function getTitle() {
 		return  WCF::getLanguage()->get($this->languageName);
+	}
+	
+	public function getSystemLanguage() {
+		return LanguageFactory::getInstance()->getLanguageByCode($this->languageCode);
 	}
 }
