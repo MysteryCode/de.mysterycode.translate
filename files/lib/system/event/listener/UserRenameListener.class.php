4608bb3cd9c1d6914e348b36990c592b85f33a08
mutec <webmaster@mysterycode.de>
2016-08-19 17:19:58 +0200

    user renaming listener

---
+++ files/lib/system/event/listener/UserRenameListener.class.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace translate\system\event\listener;
+use wcf\system\event\listener\IParameterizedEventListener;
+use wcf\system\WCF;
+
+class UserRenameListener implements IParameterizedEventListener {
+	/**
+	 * @see \wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName, array &$parameters) {
+		$objects = $eventObj->getObjects();
+		$userID = $objects[0]->userID;
+		$parameters = $eventObj->getParameters();
+		$username = $parameters['data']['username'];
+		
+		$sql = array();
+		// package versions
+		$sql[] = "UPDATE		translate" . WCF_N . "_package_version
+			SET		username = ?
+			WHERE		userID = ?";
+		
+		foreach ($sql as $query) {
+			$statement = WCF::getDB()->prepareStatement($query);
+			$statement->execute(array($username, $userID));
+		}
+	}
+}
