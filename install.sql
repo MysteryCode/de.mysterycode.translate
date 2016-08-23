32f2a858b809a0762542d9f01ecbe6e7a96d2c1e
mutec <webmaster@mysterycode.de>
2016-08-23 20:10:30 +0200

    uppercase sql types

--- install.sql
+++ install.sql
@@ -1,69 +1,69 @@
 DROP TABLE IF EXISTS translate1_package;
 CREATE TABLE translate1_package (
-	packageID					int(10)			NOT NULL	AUTO_INCREMENT,
-	identifier					varchar(255)	NOT NULL	DEFAULT '',
-	title						varchar(255)	NOT NULL,
-	description					text			NOT NULL,
-	application					tinyint(1)		NOT NULL	DEFAULT 0,
-	isunique					tinyint(1)		NOT NULL	DEFAULT 0,
-	plugin						varchar(255)	NOT NULL	DEFAULT '',
-	author						varchar(255)	NOT NULL,
-	authorUrl					varchar(255)	NOT NULL,
-	supportUrl					varchar(255)	NOT NULL,
+	packageID					INT(10)			NOT NULL	AUTO_INCREMENT,
+	identifier					VARCHAR(255)	NOT NULL	DEFAULT '',
+	title						VARCHAR(255)	NOT NULL,
+	description					TEXT			NOT NULL,
+	application					TINYINT(1)		NOT NULL	DEFAULT 0,
+	isunique					TINYINT(1)		NOT NULL	DEFAULT 0,
+	plugin						VARCHAR(255)	NOT NULL	DEFAULT '',
+	author						VARCHAR(255)	NOT NULL,
+	authorUrl					VARCHAR(255)	NOT NULL,
+	supportUrl					VARCHAR(255)	NOT NULL,
 	PRIMARY KEY (packageID)
 );
 
 DROP TABLE IF EXISTS translate1_package_version;
 CREATE TABLE translate1_package_version (
-	versionID					int(10)			NOT NULL	AUTO_INCREMENT,
-	packageID					int(10)			NOT NULL,
-	userID						int(10),
-	username					varchar(255)	NOT NULL	DEFAULT '',
-	version						varchar(255)	NOT NULL	DEFAULT '',
-	codename					varchar(255)	NOT NULL	DEFAULT '',
-	time						int(10)			NOT NULL	DEFAULT 0,
-	license						varchar(255)	NOT NULL	DEFAULT '',
-	licenseUrl					varchar(255)	NOT NULL,
-	excludedPackages			text			NOT NULL,
-	requiredPackages			text			NOT NULL,
-	optionals					text			NOT NULL,
-	updateFrom					text			NOT NULL,
-	active						tinyint(1)		NOT NULL	DEFAULT 0,
-	filename					varchar(255)	NOT NULL	DEFAULT '',
-	filesize					varchar(255)	NOT NULL	DEFAULT '',
-	tmpHash						varchar(40)		NOT NULL	DEFAULT '',
-	mimeType					varchar(255)	NOT NULL	DEFAULT '',
+	versionID					INT(10)			NOT NULL	AUTO_INCREMENT,
+	packageID					INT(10)			NOT NULL,
+	userID						INT(10),
+	username					VARCHAR(255)	NOT NULL	DEFAULT '',
+	version						VARCHAR(255)	NOT NULL	DEFAULT '',
+	codename					VARCHAR(255)	NOT NULL	DEFAULT '',
+	time						INT(10)			NOT NULL	DEFAULT 0,
+	license						VARCHAR(255)	NOT NULL	DEFAULT '',
+	licenseUrl					VARCHAR(255)	NOT NULL,
+	excludedPackages			TEXT			NOT NULL,
+	requiredPackages			TEXT			NOT NULL,
+	optionals					TEXT			NOT NULL,
+	updateFrom					TEXT			NOT NULL,
+	active						TINYINT(1)		NOT NULL	DEFAULT 0,
+	filename					VARCHAR(255)	NOT NULL	DEFAULT '',
+	filesize					VARCHAR(255)	NOT NULL	DEFAULT '',
+	tmpHash						VARCHAR(40)		NOT NULL	DEFAULT '',
+	mimeType					VARCHAR(255)	NOT NULL	DEFAULT '',
 	PRIMARY KEY (versionID)
 );
 
 DROP TABLE IF EXISTS translate1_language;
 CREATE TABLE translate1_language (
-	languageID					int(10)			NOT NULL	AUTO_INCREMENT PRIMARY KEY,
-	languageCode				varchar(20)		NOT NULL	DEFAULT '',
-	languageName				varchar(255)	NOT NULL	DEFAULT '',
-	foreignLanguageName			varchar(255)	NOT NULL	DEFAULT '',
-	countryCode					varchar(10)		NOT NULL	DEFAULT '',
-	isDisabled					tinyint(1)		NOT NULL	DEFAULT 0,
+	languageID					INT(10)			NOT NULL	AUTO_INCREMENT PRIMARY KEY,
+	languageCode				VARCHAR(20)		NOT NULL	DEFAULT '',
+	languageName				VARCHAR(255)	NOT NULL	DEFAULT '',
+	foreignLanguageName			VARCHAR(255)	NOT NULL	DEFAULT '',
+	countryCode					VARCHAR(10)		NOT NULL	DEFAULT '',
+	isDisabled					TINYINT(1)		NOT NULL	DEFAULT 0,
 	UNIQUE KEY languageCode (languageCode)
 );
 
 DROP TABLE IF EXISTS translate1_language_category;
 CREATE TABLE translate1_language_category (
-	languageCategoryID			int(10)			NOT NULL	AUTO_INCREMENT PRIMARY KEY,
-	languageCategory			varchar(191)	NOT NULL	DEFAULT '',
+	languageCategoryID			INT(10)			NOT NULL	AUTO_INCREMENT PRIMARY KEY,
+	languageCategory			VARCHAR(191)	NOT NULL	DEFAULT '',
 	UNIQUE KEY languageCategory (languageCategory)
 );
 
 DROP TABLE IF EXISTS translate1_language_item;
 CREATE TABLE translate1_language_item (
-	languageItemID				int(10)			NOT NULL	AUTO_INCREMENT PRIMARY KEY,
-	languageID					int(10)			NOT NULL,
-	languageItem				varchar(191)	NOT NULL	DEFAULT '',
-	languageItemValue			mediumtext		NOT NULL,
-	languageCategoryID			int(10)			NOT NULL,
-	checked						tinyint(1)		NOT NULL	DEFAULT 0,
-	checkedByUserIDs			mediumtext,
-	packageID					int(10),
+	languageItemID				INT(10)			NOT NULL	AUTO_INCREMENT PRIMARY KEY,
+	languageID					INT(10)			NOT NULL,
+	languageItem				VARCHAR(191)	NOT NULL	DEFAULT '',
+	languageItemValue			MEDIUMTEXT		NOT NULL,
+	languageCategoryID			INT(10)			NOT NULL,
+	checked						TINYINT(1)		NOT NULL	DEFAULT 0,
+	checkedByUserIDs			MEDIUMTEXT,
+	packageID					INT(10),
 	UNIQUE KEY languageItem (languageItem, languageID)
 );
 
