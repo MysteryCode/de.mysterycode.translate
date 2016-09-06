5c33d2fd0038a70d957ff84b581048f687709a7c
mutec <webmaster@mysterycode.de>
2016-09-06 21:47:30 +0200

    fixed category and item list

--- files/lib/data/language/category/LanguageCategory.class.php
+++ files/lib/data/language/category/LanguageCategory.class.php
@@ -4,5 +4,8 @@ namespace translate\data\language\category;
 use wcf\data\DatabaseObject;
 
 class LanguageCategory extends DatabaseObject {
-	
+	/**
+	 * @inheritdoc
+	 */
+	protected static $databaseTableIndexName = 'languageCategoryID';
 }
--- files/lib/data/language/item/LanguageItem.class.php
+++ files/lib/data/language/item/LanguageItem.class.php
@@ -4,5 +4,8 @@ namespace translate\data\language\item;
 use wcf\data\DatabaseObject;
 
 class LanguageItem extends DatabaseObject {
-	
+	/**
+	 * @inheritdoc
+	 */
+	protected static $databaseTableIndexName = 'languageItemID';
 }
