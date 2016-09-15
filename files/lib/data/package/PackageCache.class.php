3b1f0c53fb4fc70e869f2891cf0ddcafca5c4322
mutec <webmaster@mysterycode.de>
2016-09-15 20:49:41 +0200

    getPackageByIdentifier()

--- files/lib/data/package/PackageCache.class.php
+++ files/lib/data/package/PackageCache.class.php
@@ -12,12 +12,24 @@ class PackageCache extends SingletonFactory {
 	 * @var \translate\data\package\Package[]
 	 */
 	protected $cachedObjects = array();
+	
+	/**
+	 * identifier as array-key
+	 * packageID as value
+	 * 
+	 * @var integer[]
+	 */
+	protected $packageIDs = [];
 
 	/**
 	 * @see \wcf\system\SingletonFactory::init()
 	 */
 	protected function init () {
 		$this->cachedObjects = PackageCacheBuilder::getInstance()->getData(array(), 'packages');
+		
+		foreach ($this->cachedObjects as $package) {
+			$this->packageIDs[$package->identifier] = $package->packageID;
+		}
 	}
 
 	/**
@@ -41,5 +53,32 @@ class PackageCache extends SingletonFactory {
 	 */
 	public function getPackages () {
 		return $this->cachedObjects;
+	}
+	
+	/**
+	 * Returns the packageID of the package matching the given identifier string
+	 *  
+	 * @param string $identifier
+	 * @return NULL|\translate\data\package\integer
+	 */
+	public function getPackageIDByIdentifier($identifier) {
+		if (empty($this->packageIDs[$identifier]))
+			return null;
+		
+		return $this->packageIDs[$identifier];
+	}
+	
+	/**
+	 * Returns the package object matching the given identifier string
+	 * 
+	 * @param string $identifier
+	 * @return NULL|\translate\data\package\Package
+	 */
+	public function getPackageByIdentifier($identifier) {
+		$packageID = $this->getPackageIDByIdentifier($identifier);
+		if (empty($packageID) || $packageID === null)
+			return null;
+		
+		return self::getInstance()->getPackage($packageID);
 	}
 }
