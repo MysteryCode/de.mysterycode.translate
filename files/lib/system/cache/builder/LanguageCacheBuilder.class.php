4a6cdecb8706bbcde33e6c9da7568ea335d85c1f
mutec <webmaster@mysterycode.de>
2016-09-19 14:34:54 +0200

    fixed lang cache renewal

--- files/lib/system/cache/builder/LanguageCacheBuilder.class.php
+++ files/lib/system/cache/builder/LanguageCacheBuilder.class.php
@@ -21,15 +21,15 @@ class LanguageCacheBuilder extends AbstractCacheBuilder {
 		$data['languages'] = $languageList->getObjects();
 		
 		// set stat columns on languages
-		$sql = "UPDATE translate" . WCF_N . "_language
+		$sql = "UPDATE translate" . WCF_N . "_language language
 			SET variables = (
 				SELECT COUNT(language_item_value.languageItemValueID)
 				FROM translate" . WCF_N . "_language_item_value language_item_value
-				WHERE language_item_value.languageID = languageID
+				WHERE language_item_value.languageID = language.languageID
 			), variablesChecked = (
 				SELECT COUNT(language_item_value2.languageItemValueID)
 				FROM translate" . WCF_N . "_language_item_value language_item_value2
-				WHERE language_item_value2.languageID = languageID
+				WHERE language_item_value2.languageID = language.languageID
 					AND language_item_value2.checked = ?
 			)";
 		$statement = WCF::getDB()->prepareStatement($sql);
