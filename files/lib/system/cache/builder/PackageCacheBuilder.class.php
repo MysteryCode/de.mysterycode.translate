d14c9d354a74d4550cb9eb12c088025326288d2f
mutec <webmaster@mysterycode.de>
2016-09-19 14:27:49 +0200

    update package stats

--- files/lib/system/cache/builder/PackageCacheBuilder.class.php
+++ files/lib/system/cache/builder/PackageCacheBuilder.class.php
@@ -20,6 +20,29 @@ class PackageCacheBuilder extends AbstractCacheBuilder {
 		$packageList->readObjects();
 		$data['packages'] = $packageList->getObjects();
 		
+		// set stat columns on languages
+		$sql = "UPDATE translate" . WCF_N . "_package package
+			SET variables = (
+				SELECT COUNT(language_item_value.languageItemValueID)
+				FROM translate" . WCF_N . "_language_item_value language_item_value
+				WHERE language_item_value.languageItemID IN (
+					SELECT language_item.languageItemID
+					FROM translate" . WCF_N . "_language_item language_item
+					WHERE language_item.packageID = package.packageID
+				)
+			), variablesChecked = (
+				SELECT COUNT(language_item_value2.languageItemValueID)
+				FROM translate" . WCF_N . "_language_item_value language_item_value2
+				WHERE language_item_value2.languageItemID IN (
+					SELECT language_item2.languageItemID
+					FROM translate" . WCF_N . "_language_item language_item2
+					WHERE language_item2.packageID = package.packageID
+				)
+					AND language_item_value2.checked = ?
+			)";
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute([ 1 ]);
+		
 		return $data;
 	}
 }
