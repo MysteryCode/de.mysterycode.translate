d9e400c0671936ddccb23f36114e2afc94bafdcf
mutec <webmaster@mysterycode.de>
2016-09-19 14:32:41 +0200

    reduced queries to update language stats

--- files/lib/system/cache/builder/LanguageCacheBuilder.class.php
+++ files/lib/system/cache/builder/LanguageCacheBuilder.class.php
@@ -20,6 +20,21 @@ class LanguageCacheBuilder extends AbstractCacheBuilder {
 		$languageList->readObjects();
 		$data['languages'] = $languageList->getObjects();
 		
+		// set stat columns on languages
+		$sql = "UPDATE translate" . WCF_N . "_language
+			SET variables = (
+				SELECT COUNT(language_item_value.languageItemValueID)
+				FROM translate" . WCF_N . "_language_item_value language_item_value
+				WHERE language_item_value.languageID = languageID
+			), variablesChecked = (
+				SELECT COUNT(language_item_value2.languageItemValueID)
+				FROM translate" . WCF_N . "_language_item_value language_item_value2
+				WHERE language_item_value2.languageID = languageID
+					AND language_item_value2.checked = ?
+			)";
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute([ 1 ]);
+		
 		return $data;
 	}
 }
